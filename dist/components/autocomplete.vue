c0bdbc40403f84dac209f5d66de92849ebb9ad66
Raymond Torino <raymond.torino@wavecell.com>
2019-02-28 17:10:14 +0800

    fix error

---
+++ dist/components/autocomplete.vue
@@ -0,0 +1,12 @@
+<template>
+  <input
+    ref="input"
+    v-bind="$attrs"
+    v-on="$listeners"
+    />
+</template>
+
+<script>
+import autocompleteImpl from './autocompleteImpl'
+export default (function (x) { return x.default || x })(autocompleteImpl)
+</script>
---
+++ dist/components/infoWindow.vue
@@ -0,0 +1,15 @@
+/* vim: set softtabstop=2 shiftwidth=2 expandtab : */
+
+<template>
+  <div>
+    <div ref="flyaway"> <!-- so named because it will fly away to another component -->
+      <slot>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import infoWindowImpl from './infoWindowImpl'
+export default (function (x) { return x.default || x })(infoWindowImpl)
+</script>
